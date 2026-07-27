cfd1f69bb31bea8612852e0e2009f80731f534ee
Bikash sah <bikashcod@gmail.com>
2026-07-28 02:41:53 +0530

    Intital commit:

---
+++ src/main/java/com/InsightIQ/InsightIQ/InsightIqApplication.java
@@ -0,0 +1,13 @@
+package com.InsightIQ.InsightIQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightIqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InsightIQ/InsightIQ/InsightIqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InsightIQ.InsightIQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightIqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
