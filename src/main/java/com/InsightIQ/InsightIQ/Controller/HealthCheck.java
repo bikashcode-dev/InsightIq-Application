2b9e3612c15ac398418d8297a37b3df05f3803b9
Bikash sah <bikashcod@gmail.com>
2026-07-28 13:25:04 +0530

    add initial project structure and api chaek

---
+++ src/main/java/com/InsightIQ/InsightIQ/Controller/HealthCheck.java
@@ -0,0 +1,17 @@
+package com.InsightIQ.InsightIQ.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Health")
+public class HealthCheck {
+
+    @GetMapping
+    public String HealthCheck() {
+        return "Hello World Bikash";
+    }
+
+}
--- src/main/java/com/InsightIQ/InsightIQ/InsightIqApplication.java
+++ src/main/java/com/InsightIQ/InsightIQ/InsightIqApplication.java
@@ -8,6 +8,7 @@ public class InsightIqApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InsightIqApplication.class, args);
+        System.out.println("hello ");
 	}
 
 }
