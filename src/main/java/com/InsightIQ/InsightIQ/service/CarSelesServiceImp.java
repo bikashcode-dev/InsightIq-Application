de3083084f7b48b70ffa56d2eeed1eeb5e6cdaba
Bikash sah <bikashcod@gmail.com>
2026-08-03 12:52:08 +0530

    add globl exption handler

--- src/main/java/com/InsightIQ/InsightIQ/service/CarSelesServiceImp.java
+++ src/main/java/com/InsightIQ/InsightIQ/service/CarSelesServiceImp.java
@@ -12,6 +12,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,6 +32,7 @@ public class CarSelesServiceImp implements CarService {
     public UploadSalesResponse uploadCsv(MultipartFile file) {
 
 
+        // add list ROW data
         List<CarSaleEntity> carSaleEntities = new ArrayList<CarSaleEntity>();
 
         // if row fail count varible
@@ -71,30 +73,27 @@ public class CarSelesServiceImp implements CarService {
 
                // set deatisl by entitty hat tari ki
                CarSaleEntity carSales = new CarSaleEntity();
+
                carSales.setCarNumber(record.get("Car Number"));
                carSales.setCarNumber(record.get("Brand"));
                carSales.setCarNumber(record.get("Model"));
                carSales.setCarNumber(record.get("Color"));
-               carSales.setCarNumber(record.get("Year"));
-               carSales.setCarNumber(record.get("Date of Purchase"));
-               carSales.setCarNumber(record.get("Time of Purchase"));
-               carSales.setCarNumber(record.get("Price (RS)"));
-               carSales.setCarNumber(record.get("Mileage (KM/l)"));
-               carSales.setCarNumber(record.get("Engine (cc)"));
-               carSales.setCarNumber(record.get("Fuel Type"));
-               carSales.setCarNumber(record.get("Payment Mode"));
-               carSales.setCarNumber(record.get("Car Number"));
-               carSales.setCarNumber(record.get("Car Number"));
-               carSales.setCarNumber(record.get("Car Number"));
-               carSales.setCarNumber(record.get("Car Number"));
-               carSales.setCarNumber(record.get("Car Number"));
-               carSales.setCarNumber(record.get("Car Number"));
-               carSales.setCarNumber(record.get("Car Number"));
-               carSales.setCarNumber(record.get("Car Number"));
-               carSales.setCarNumber(record.get("Car Number"));
-               carSales.setCarNumber(record.get("Car Number"));
-               carSales.setCarNumber(record.get("Car Number"));
-               carSales.setCarNumber(record.get("Car Number"));
+               carSales.setYear(Integer.parseInt(record.get("Year")));
+               carSales.setTimeOfPurchaseDate(LocalDate.parse(record.get("Date of Purchase")));
+               carSales.setTimeOfPurchaseDate(LocalDate.parse(record.get("Time of Purchase")));
+               carSales.setPrice(Long.parseLong(record.get("Price (RS)")));
+               carSales.setMileage(Double.parseDouble(record.get("Mileage (KM)")));
+               carSales.setEngine(Integer.parseInt(record.get("Engine (CC)")));
+               carSales.setFuelType(record.get("Fuel Type"));
+               carSales.setPaymentMode(record.get("Payment Mode"));
+               carSales.setState(record.get("State"));
+               carSales.setCity(record.get("City"));
+               carSales.setCountry(record.get("Country"));
+               carSales.setCustomerName(record.get("Customer Name"));
+               carSales.setCustomerPhoneNumber(record.get("Customer Phone Number"));
+               carSales.setCustomerEmail(record.get("Customer Email"));
+               carSales.setWarrantyPeriod(record.get("Warranty Period"));
+               carSaleEntities.add(carSales);
             }
         }
 
